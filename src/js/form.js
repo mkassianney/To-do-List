353f492f265454196cef994081730549c538e8d3
Maria Eduarda Kassianney da Silva <eduardakassianney..wallon@gmail.com>
2025-09-11 10:27:11 -0300

    feat: task text effect

--- src/js/form.js
+++ src/js/form.js
@@ -3,6 +3,8 @@ const showFormBtn = document.getElementById("showFormBtn");
 const addTaskBtnContainer = document.getElementById("addTaskBtnContainer");
 const formContainer = document.getElementById("formContainer");
 const cancelBtn = document.getElementById("cancelBtn"); // cancel button in form
+const checkBoxes = document.querySelectorAll(".task-checkbox");
+const taskName = document.querySelector(".task-name");
 
 // Show form when you click on "Add new task"
 showFormBtn.addEventListener("click", () => {
@@ -14,4 +16,17 @@ showFormBtn.addEventListener("click", () => {
 cancelBtn.addEventListener("click", () => {
   formContainer.classList.add("hidden");       // hide form
   addTaskBtnContainer.classList.remove("hidden"); // show button again
+});
+
+checkBoxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    // pega o elemento <p> que está logo ao lado da checkbox
+    const taskName = checkbox.nextElementSibling;
+
+    if (checkbox.checked) {
+      taskName.classList.add("line-through", "text-gray-400");
+    } else {
+      taskName.classList.remove("line-through", "text-gray-400");
+    }
+  });
 });
